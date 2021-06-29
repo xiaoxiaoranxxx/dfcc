44a9ac8816988283b5605446b37cfd49b5e8d7bc
xiaoxiaoran <2425940130@qq.com>
2021-06-29 15:41:36 +0800

    shell

--- xiaoxiaoran1/shell/1.sh
+++ xiaoxiaoran1/shell/1.sh
@@ -2,6 +2,7 @@
 
 
 ping -c1 baidu.com && echo "xiaoxiaoran"
+ping -c1 baidu.com &>/dev/null && echo "xiaoxiaoran" || echo die
 
 /usr/bin/python <<@@
 print "xiaoxiao"
@@ -13,3 +14,39 @@ mkdir -pv /home/{333/{aaa,sss},444}
 echo \*      #转义
 touch 1.txt 2.txt
 touch 1.txt\ 2.txt
+
+echo -e "\e[1;31mthis is red"
+echo -e "\e[1;0mthis is null"
+echo -e "\e[1;31mthis is red.\e[0m"
+echo -e "\e[1;41mthis is red_b.\e[0m"
+
+ip=192.168.0.1
+ping -c2 $ip
+
+if ping -c2 $ip &>/dev/null;then 
+	echo "$ip is up"
+else 
+	echo "$ip is down"
+fi
+
+ping -c2 $ip &>/dev/null  
+if[ $? -eq 0 ] ;then 
+	echo "$ip is up"
+else 
+	echo "$ip is down"
+fi
+
+read -p "please input a ip-->"  ip
+ping -c2 $ip 
+if[ $? -eq 0 ] ; then 
+	echo "$ip is up"
+else 
+	echo "$ip is down"
+fi
+
+echo $1 $2
+
+export  xiao=1 #环境变量
+unset  xiao
+
+
