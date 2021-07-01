d5c4f6bc8119ddbb5d9863f4a23a6d91a0488e5e
xiaoxiaoran <2425940130@qq.com>
2021-07-01 14:42:07 +0800

    shell

---
+++ xiaoxiaoran1/shell/3.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/bash
+clear
+
+#trap "" HUP INT OUIT TSTP
+
+cat <<-@@
+	+-------------------+
+	|   xiaoxiaoran     |
+	| 	1. ping         |
+	| 	2. disk         |
+	| 	3. filesystem   |
+	| 	4. memory       |
+	|	5. system       | 
+	|   6. xiuxiu       |
+	+-------------------+
+@@
+while :
+do
+	
+	#read -p "please input a num-->" num
+	echo -en "\e[1;32minput a num--> \e[0m"
+	read num
+	case "$num" in 
+	1) 
+		echo "ping-ping-ping"
+		ping -c1 127.0.0.1
+		;;
+	2) 
+		echo disk
+		fdisk -l
+		;;
+	3) 
+		echo df
+		df -Th
+		;;
+	4) 
+		echo free
+		free -m
+		;;
+	5)
+		echo "xiaoxiaoran666"
+		;;
+	exit)
+		exit 
+		;;
+	"")
+		;;
+	*)
+		echo "error ..."
+	esac
+done
